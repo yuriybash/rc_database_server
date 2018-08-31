fc70a33d8754bf173bcbaa54f996e074844cae93
Yuriy Bash <yuriybash@gmail.com>
2018-08-31 15:20:42 -0500

    adds bad param test

--- tests/handlers.py
+++ tests/handlers.py
@@ -7,13 +7,19 @@ from handlers import RequestHandler
 class RequestHandlerTest(unittest.TestCase):
 
 
-    def test_parse_good_path(self):
+    def test_parse_good_path_good_params(self):
 
         mock_handler = self._create_mock_handler('/set?somekey=somevalue')
         parsed_path, params = mock_handler.parse()
         self.assertEqual('set', parsed_path)
         self.assertEqual({'somekey': ['somevalue']}, params)
 
+    @mock.patch.object(RequestHandler, 'handle_error')
+    def test_parse_good_path_bad_params(self, mock_call):
+
+        mock_handler = self._create_mock_handler('/get?badparam=badval')
+        with self.assertRaises(ValueError):
+            mock_handler.parse()
 
     @mock.patch.object(RequestHandler, 'handle_error')
     def test_parse_bad_path(self, mock_call):
