2168e5d3e9c9ee9c3e44259419d72d641d2f5073
Yuriy Bash <yuriybash@gmail.com>
2018-08-31 15:53:25 -0500

    set x bit

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from handlers import RequestHandler
 DEFAULT_PORT = 4000
 
 def run_server():
+    """
+    Run the server. Uses a default port, but allows the usage of a different
+    port via an additional positional argument.
+
+    :return: None
+    """
     port = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PORT
     address = ('127.0.0.1', port)
     httpd = HTTPServer(address, RequestHandler)
