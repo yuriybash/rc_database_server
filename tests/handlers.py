7384276cd2b7b2424b73fd0c6e56a0c60d37c1e2
Yuriy Bash <yuriybash@gmail.com>
2018-08-31 15:39:27 -0500

    completes tests

--- tests/handlers.py
+++ tests/handlers.py
@@ -3,6 +3,7 @@ import mock
 import unittest
 
 from handlers import RequestHandler
+from storage import InMemoryStorage
 
 class RequestHandlerTest(unittest.TestCase):
 
@@ -28,6 +29,44 @@ class RequestHandlerTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             mock_handler.parse()
 
+    def test_validate_good_path(self):
+        mock_handler = self._create_mock_handler('/set?somekey=somevalue')
+        mock_handler.parse()
+
+    @mock.patch.object(RequestHandler, 'handle_error')
+    def test_validate_bad_path(self, mock_call):
+        mock_handler = self._create_mock_handler('/')
+        with self.assertRaises(ValueError):
+            mock_handler.parse()
+
+        mock_handler = self._create_mock_handler('/bad_path')
+        with self.assertRaises(ValueError):
+            mock_handler.parse()
+
+        # missing queryparams
+        mock_handler = self._create_mock_handler('/set')
+        with self.assertRaises(ValueError):
+            mock_handler.parse()
+
+        # missing queryparams
+        mock_handler = self._create_mock_handler('/get')
+        with self.assertRaises(ValueError):
+            mock_handler.parse()
+
+    @mock.patch.object(RequestHandler, 'handle_ok')
+    def test_do_GET(self, mock_call):
+
+        mock_handler = self._create_mock_handler('/set?fruit=apple')
+        mock_handler.do_GET()
+        mock_call.assert_called_with('OK\n')
+
+        mock_handler = self._create_mock_handler('/get?key=fruit')
+        mock_handler.do_GET()
+        mock_call.assert_called_with('apple\n')
+
+
+    def tearDown(cls):
+        InMemoryStorage.CONTAINER = {}
 
     def _create_mock_handler(self, path):
 
